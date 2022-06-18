a6210a074982d065589eb13874c2b597f7a93eb6
Marko Perica <marko.perica2@gmail.com>
2022-06-18 19:30:38 +0200

    added inputs

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 // component imports
 import Header from "./components/Header";
-import Main from "./components/Main";
+import Portfolio from "./components/Portfolio";
 import Controls from "./components/Controls";
 import Blogs from "./components/Blogs";
 import Test from "./components/Test";
+import { Link, NavLink } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
 
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
@@ -16,14 +20,19 @@ import "./styles/styles.scss";
 function App() {
   return (
     <React.Fragment>
-      <div className="App"></div>
+      <div className="App">
       <Router>
         <Routes>
           <Route exact path="/" element={<Header />} />
+          <Route path="/portfolio" element={<Portfolio />}></Route>
           <Route path="/blogs" element={<Blogs />}></Route>
+          <Route path="/about" element={<About/>}></Route>
+          <Route path="/contact" element={<Contact/>} ></Route>
         </Routes>
-      </Router>
       <Controls></Controls>
+      </Router>
+      </div>
+      
     </React.Fragment>
   );
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,121 @@
-import React from 'react';
+import React from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faMapMarker,
+  faUserGraduate,
+  faMobile,
+  faGlobeEurope,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Contact() {
-    return <React.Fragment>
-        <h1>Contact</h1>
+  return (
+    <React.Fragment>
+      <section className="container contact" id="contact">
+        <div className="contact-container">
+          <div className="main-title">
+            <h2>
+              Contact <span>Me</span>
+              <span className="bg-text">Contact</span>
+            </h2>
+          </div>
+          <div className="contact-content-con">
+            <div className="left-contact">
+              <h4>Contact me here</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. In,
+                laborum numquam? Quam excepturi perspiciatis quas quasi.
+              </p>
+              <div className="contact-info">
+                <div className="contact-item">
+                  <div className="icon">
+                    <FontAwesomeIcon icon={faMapMarker}></FontAwesomeIcon>
+                    <span>Location</span>
+                  </div>
+                  <p>: London, united Kingdom</p>
+                </div>
+                <div className="contact-item">
+                  <div className="icon">
+                    <FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>
+                    <span>Email</span>
+                  </div>
+                  <p>
+                    <span>: marko.perica@markoperica.com.hr</span>
+                  </p>
+                </div>
+                <div className="contact-item">
+                  <div className="icon">
+                    <FontAwesomeIcon icon={faUserGraduate}></FontAwesomeIcon>
+                    <span>Education</span>
+                  </div>
+                  <p>
+                    <span>: FERIT Osijek</span>
+                  </p>
+                </div>
+                <div className="contact-item">
+                  <div className="icon">
+                    <FontAwesomeIcon icon={faMobile}></FontAwesomeIcon>
+                    <span>Mobile Number</span>
+                  </div>
+                  <p>
+                    <span>: +385977580825</span>
+                  </p>
+                </div>
+                <div className="contact-item">
+                  <div className="icon">
+                    <FontAwesomeIcon icon={faGlobeEurope}></FontAwesomeIcon>
+                    <span>Languages</span>
+                  </div>
+                  <p>
+                    <span>: English, Croatian, Italian</span>
+                  </p>
+                </div>
+              </div>
+              <div className="contact-icons">
+                <div className="contact-icon">
+                  <a href="www.facebook.com" target="_blank">
+                    <i className="fab fa-facebook-f"></i>
+                  </a>
+                  <a href="#" target="_blank">
+                    <i className="fab fa-twitter"></i>
+                  </a>
+                  <a href="#" target="_blank">
+                    <i className="fab fa-github"></i>
+                  </a>
+                  <a href="#" target="_blank">
+                    <i className="fab fa-youtube"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="right-contact">
+              <form action="">
+                <div className="input-control i-c-2">
+                  <input type="text" required placeholder="YOUR NAME" />
+                  <input type="email" required placeholder="YOUR EMAIL" />
+                </div>
+                <div className="input-control">
+                  <textarea
+                    name=""
+                    id=""
+                    cols="15"
+                    rows="8"
+                    placeholder="Message here..."
+                  ></textarea>
+                </div>
+                <div className="submit-btn">
+                  <a href="" className="main-btn">
+                    <span className="btn-text">Submit</span>
+                  </a>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
     </React.Fragment>
+  );
 }
 
 export default Contact;
